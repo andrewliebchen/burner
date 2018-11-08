75d52f5758fdbd8a526f99da6567ba27f56153bc
Andrew Liebchen <andrewliebchen@gmail.com>
2018-11-07 19:51:05 -0800

    Stub out basic page

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,11 +1,58 @@
 import React from 'react';
-import {withTracker} from 'react-meteor-data';
-import {Tasks} from '../api/canvases';
+import {createContainer} from 'react-meteor-data';
+import {Canvases} from '../api/canvases';
+import {Elements} from '../api/elements';
+import PropTypes from 'prop-types';
+import {Box, Button, Flex, Text} from 'rebass';
+import {ChevronRight} from 'react-feather';
 
 const App = props => (
-  <div>
-    <h1>Hello</h1>
-  </div>
+  <Flex flexDirection="column">
+    <Flex width={1} p={2} alignItems="center">
+      <Text p={2}>Projects</Text>
+      <ChevronRight />
+      <Text p={2}>Canvases</Text>
+      <ChevronRight />
+      <Text p={2}>Canvas</Text>
+      <Text p={2}>Workshop</Text>
+      <Text p={2}>Pair & Share</Text>
+    </Flex>
+    <Flex width={1} p={3}>
+      <Box>
+        <Text mb={1}>Problem Statement</Text>
+        <Text fontSize={4}>
+          Exercitation incididunt est minim exercitation velit ipsum Lorem
+          mollit qui consectetur quis.
+        </Text>
+      </Box>
+    </Flex>
+    <Flex width={1}>
+      <Flex width={1 / 5} p={3} flexDirection="column">
+        <Box p={2} bg="blue">
+          <Text color="white">Impacts</Text>
+        </Box>
+        <Box p={2}>
+          <Text>Success Measures</Text>
+        </Box>
+        <Box p={2}>
+          <Text>Scenarios</Text>
+        </Box>
+      </Flex>
+      <Flex width={4 / 5} p={3}>
+        Main
+      </Flex>
+    </Flex>
+  </Flex>
 );
 
-export default App;
+App.propTypes = {
+  canvases: PropTypes.array,
+  elements: PropTypes.array,
+};
+
+export default createContainer(() => {
+  return {
+    canvases: Canvases.find({}).fetch(),
+    elements: Elements.find({}).fetch(),
+  };
+}, App);
