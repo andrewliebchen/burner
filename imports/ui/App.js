e356a965e89f2d06559ce13bd163b266168aa922
Andrew Liebchen <andrewliebchen@gmail.com>
2018-11-07 18:21:19 -0800

    Init

---
+++ imports/ui/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {withTracker} from 'react-meteor-data';
+import {Tasks} from '../api/canvases';
+
+const App = props => (
+  <div>
+    <h1>Hello</h1>
+  </div>
+);
+
+export default App;
