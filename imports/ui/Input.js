16a2e6563e4bad9210f387eb7620c6d6b3565085
Andrew Liebchen <andrewliebchen@gmail.com>
2018-12-16 19:05:24 -0800

    Input styling

--- imports/ui/Input.js
+++ imports/ui/Input.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import {Flex, Text} from 'rebass';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Flex, Text } from "rebass";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Element = styled.input`
+  appearance: none;
+  border: 0;
+  border-bottom: 1px solid;
+  padding: 0 0 0.5em 0;
+  margin-bottom: -0.5em;
+  font-size: 1em;
+
+  &:focus {
+    border-color: blue;
+    outline: 0;
+  }
+`;
 
 const Input = props => (
   <Flex flexDirection="column" width={1}>
     <Text>{props.label}</Text>
-    <input type="text" {...props} />
+    <Element type="text" {...props} />
   </Flex>
 );
 
 Input.propTypes = {
-  label: PropTypes.string,
+  label: PropTypes.string
 };
 
 export default Input;
